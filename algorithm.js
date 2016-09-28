19ee0d380cc6fa96bc2b3a905c435094bcca1160
MiaChi <purple5world@gmail.com>
2016-09-28 17:15:32 +0800

    continuous vector which sum is max

--- algorithm.js
+++ algorithm.js
@@ -2,9 +2,54 @@ var a = 8;
 var b = [6,2,4,1,5,9];
 //search(0,8);
 //bubbleSort(b);
-quickSort(0,5,b);
-b.forEach(function(item,index){console.log(item)});
+//quickSort(0,5,b);
+//b.forEach(function(item,index){console.log(item)});
 
+//var vector=[31,-41,59,26,-53,58,97,-93,-23,84];
+var v=[31,-41,59,26,-53,58,97,-93,-23,84];
+//maxSum(vector);
+var result=maxSumOpt(0,9);
+console.log(result);
+
+//连续子向量最大和 O(n3)
+function maxSum(v){
+	var sum = 0;
+	var bigsum  = 0;
+  for(var i=0;i<v.length;i++){
+    for(var j=i;j<v.length;j++){
+		  sum=0;
+			for(var k=i;k<j;k++){
+        sum += v[k]; 
+			}
+	bigsum = bigsum > sum ? bigsum:sum;
+		}	
+	}
+	console.log('max sum is:'+bigsum);
+}
+//优化－分治算法,递归 O(nlogn)
+function maxSumOpt(l,r){
+	if(l>r) {return 0;};
+	if(l==r) {return v[l]<0?0:v[l];}
+	var m=Math.floor((l+r)/2);
+  var lmax=0;
+	var sum=0;
+	for(var i=m;i>=l;i--){
+    sum += v[i];
+		lmax = lmax>sum?lmax:sum;
+	}
+	var rmax=0;
+	sum=0;
+	for(var j=m+1;j<=r;j++){
+    sum+=v[j];
+		rmax = rmax>sum?rmax:sum;
+	}
+	return max(lmax+rmax,maxSumOpt(l,m),maxSumOpt(m+1,r));
+}
+function max(a,b,c){
+	if(c>=a && c>=b){return c;};
+	if(b>=a && b>=c){return b;};
+	if(a>=b && a>=c){return a;};
+}
 function bubbleSort(b){
   for(var i=0;i<b.length;i++){
     for(var j=0;j<b.length-i;j++){
